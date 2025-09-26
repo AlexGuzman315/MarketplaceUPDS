0bb17aee5e5e617f1c2035107745d743f24c35fc
jesus justiniano <jesus.g.justiniano@gmail.com>
2025-09-26 10:31:39 -0400

    Actualizar proyecto con PostgreSQL y login

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,11 @@
-const mysql = require("mysql2/promise");
+const { Pool } = require("pg");
 
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT || 5432
 });
 
 module.exports = pool;
