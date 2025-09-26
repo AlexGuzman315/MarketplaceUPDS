f84299a75a3eb4235d157af817b5d829aeb15096
JESUS53787 <110703109+JESUS53787@users.noreply.github.com>
2025-09-26 09:41:06 -0400

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
-// Configuración de la base de datos
 const pool = mysql.createPool({
-  host: "localhost",   // Cambia si tu BD está en otro servidor
-  user: "root",        // Tu usuario de MySQL
-  password: "123",        // Tu contraseña
-  database: "marketplaceupds", // Nombre de tu base de datos
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
